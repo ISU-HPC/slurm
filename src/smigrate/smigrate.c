754071b794bb63bffdf385999cb2761f3a69542e
Manuel Rodríguez-Pascual <supermanue@gmail.com>
2015-11-04 15:03:58 -0800

    smigrate

---
+++ src/smigrate/smigrate.c
@@ -0,0 +1,137 @@
+/*****************************************************************************\
+ *  srun.c - user interface to allocate resources, submit jobs, and execute
+ *	parallel jobs.
+ *****************************************************************************
+ *  Copyright (C) 2002-2007 The Regents of the University of California.
+ *  Copyright (C) 2008-2010 Lawrence Livermore National Security.
+ *  Produced at Lawrence Livermore National Laboratory (cf, DISCLAIMER).
+ *  Written by Mark Grondona <grondona@llnl.gov>, et. al.
+ *  CODE-OCEC-09-009. All rights reserved.
+ *
+ *  This file is part of SLURM, a resource management program.
+ *  For details, see <http://slurm.schedmd.com/>.
+ *  Please also read the included file: DISCLAIMER.
+ *
+ *  SLURM is free software; you can redistribute it and/or modify it under
+ *  the terms of the GNU General Public License as published by the Free
+ *  Software Foundation; either version 2 of the License, or (at your option)
+ *  any later version.
+ *
+ *  In addition, as a special exception, the copyright holders give permission
+ *  to link the code of portions of this program with the OpenSSL library under
+ *  certain conditions as described in each individual source file, and
+ *  distribute linked combinations including the two. You must obey the GNU
+ *  General Public License in all respects for all of the code used other than
+ *  OpenSSL. If you modify file(s) with this exception, you may extend this
+ *  exception to your version of the file(s), but you are not obligated to do
+ *  so. If you do not wish to do so, delete this exception statement from your
+ *  version.  If you delete this exception statement from all source files in
+ *  the program, then also delete it here.
+ *
+ *  SLURM is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *  FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ *  details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with SLURM; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA.
+\*****************************************************************************/
+
+#ifdef HAVE_CONFIG_H
+#  include "config.h"
+#endif
+
+#ifdef WITH_PTHREADS
+#  include <pthread.h>
+#endif
+
+#ifdef HAVE_AIX
+#  undef HAVE_UNSETENV
+#  include <sys/checkpnt.h>
+#endif
+#ifndef HAVE_UNSETENV
+#  include "src/common/unsetenv.h"
+#endif
+
+#include <sys/param.h>
+#include <sys/resource.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/utsname.h>
+#include <sys/wait.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <termios.h>
+#include <unistd.h>
+#include <grp.h>
+
+#include <slurm/slurm.h>
+
+#include "src/common/plugstack.h"
+#include "src/common/read_config.h"
+#include "src/common/xstring.h"
+
+
+#include "src/plugins/slurmctld/migration/migration.h"
+#include "opt.h"
+
+static int   _slurm_debug_env_val (void);
+
+
+int main(int argc, char *argv[])
+{
+
+  char *script_name;
+  void *script_body;
+  int script_size = 0;
+  log_options_t logopt = LOG_OPTS_STDERR_ONLY;
+
+
+  slurm_conf_init(NULL);
+	log_init(xbasename(argv[0]), logopt, 0, NULL);
+
+	script_name = process_options_first_pass(argc, argv);
+	if (opt.verbose || opt.quiet) {
+		logopt.stderr_level += opt.verbose;
+		logopt.stderr_level -= opt.quiet;
+		logopt.prefix_level = 1;
+		log_alter(logopt, 0, NULL);
+	}
+
+	if (process_options_second_pass(
+				(argc),
+				argv,
+				script_name ? xbasename (script_name) : "stdin",
+				script_body, script_size) < 0) {
+		error("smigrate parameter parsing");
+		exit(error_exit);
+	}
+
+
+  slurm_checkpoint_migrate ( opt.jobid, opt.nodes);
+
+  return (0);
+}
+
+
+
+static int _slurm_debug_env_val (void)
+{
+	long int level = 0;
+	const char *val;
+
+	if ((val = getenv ("SLURM_DEBUG"))) {
+		char *p;
+		if ((level = strtol (val, &p, 10)) < -LOG_LEVEL_INFO)
+			level = -LOG_LEVEL_INFO;
+		if (p && *p != '\0')
+			level = 0;
+	}
+	return ((int) level);
+}
