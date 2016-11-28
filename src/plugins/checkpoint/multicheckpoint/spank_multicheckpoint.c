c224370323dbd5b39a70e08e231e506365c1650f
Manuel Rodríguez-Pascual <supermanue@gmail.com>
2016-11-28 09:20:43 +0100

    cleaning of tmp files in checkpoint

--- src/plugins/checkpoint/multicheckpoint/spank_multicheckpoint.c
+++ src/plugins/checkpoint/multicheckpoint/spank_multicheckpoint.c
@@ -31,7 +31,6 @@ SPANK_PLUGIN(multicheckpoint_spank, 3);
  */
 
 static int multicheckpoint_enabled=1;
-static const uint32_t default_dmtcp_port=7779; // DMTCP default port.
 static const char cr_checkpoint_start[] = PKGLIBEXECDIR "/cr_start.sh";
 
 static int _enable_multicheckpoint (int val, const char *optarg, int remote);
@@ -54,39 +53,34 @@ struct spank_option spank_options[] =
  */
 int slurm_spank_init (spank_t sp, int ac, char **av)
 {
-   info("checkpoint/multicheckpoint_spank init");
    spank_option_register (sp, spank_options);
    return (ESPANK_SUCCESS);
 }
 
 
+
+//this one is called before each mpiexec call, and before starting the script
+
  int slurm_spank_task_init(spank_t sp, int ac, char **av){
-   info("checkpoint/multicheckpoint_spank slurm_spank_task_init");
    if (multicheckpoint_enabled != 0)
     return (ESPANK_SUCCESS);
 
     //we modify the application to be executed by including a DMTCP wrapper.
   char **argv;
   char **newArgv;
-  uint32_t aux;
-  char numCPUs[16];
   uint32_t argc = 0;
   uint32_t cont = 0;
 
   spank_get_item (sp, S_JOB_ARGV, &argc,&argv);
-  spank_get_item (sp, S_JOB_NCPUS,aux);
 
-  sprintf(numCPUs, "%u", aux);
-
-  argc += 2;
-  newArgv = xmalloc (sizeof(char*) * (argc + 2));
+  argc += 1;
+  newArgv = xmalloc (sizeof(char*) * (argc + 1));
   newArgv[0] = strdup(cr_checkpoint_start);
-  newArgv[1] = numCPUs;
 
 
   //slurm_error("printing newArgv");
-  for (cont = 0; cont < argc-2; cont++) {
-    newArgv[cont+2] = strdup(argv[cont]);
+  for (cont = 0; cont < argc-1; cont++) {
+    newArgv[cont+1] = strdup(argv[cont]);
   //  slurm_error(newArgv[cont+1]);
   }
   //newArgv[argc] = NULL;
@@ -102,32 +96,9 @@ return (ESPANK_SUCCESS);
 
 
 
-int slurm_spank_task_exit(spank_t sp, int ac, char **av){
-
-  slurm_error("HI EVERYONE, I AM slurm_spank_task_exit");
-
-  //job id, needed to access the rest of information
-  uint32_t job_id;
-  if (spank_get_item (sp, S_JOB_ID, &job_id) != 0){
-    slurm_error("Could not get job id");
-    return (ESPANK_ERROR);
-  }
-
-  char *ckpt_dir;
-  if (spank_get_item (sp, S_CHECKPOINT_DIR, &ckpt_dir) != 0){
-    slurm_error("Could not get checkpoint dir");
-    return (ESPANK_ERROR);
-  }
-
-  char ckpt_file[MAX_PATH_LEN];
-  sprintf(ckpt_file,"%s/%d/multicheckpoint_coordinator", ckpt_dir, job_id);
-  remove (ckpt_file);
-
-  return (ESPANK_SUCCESS);
- }
 
 static int _enable_multicheckpoint (int val, const char *optarg, int remote)
 {
     multicheckpoint_enabled=0;
-    return (0);
+    return (ESPANK_SUCCESS);
 }
