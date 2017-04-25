792f53df3086f442a8cff2a4dd9a95362c7a657a
root <root@acme31.ciemat.es>
2017-04-26 00:23:11 +0200

    tests on dmtcp driver

--- src/plugins/checkpoint/dmtcp/checkpoint_dmtcp.c
+++ src/plugins/checkpoint/dmtcp/checkpoint_dmtcp.c
@@ -542,7 +542,7 @@ extern int slurm_ckpt_signal_tasks(stepd_step_rec_t *job, char *image_dir)
 extern int slurm_ckpt_restart_task(stepd_step_rec_t *job,
 				   char *image_dir, int gtid)
 {
-	char *argv[11];
+	char *argv[10];
 
 	char context_file[MAX_PATH_LEN];
 	/* jobid and stepid must NOT be spelled here,
@@ -560,13 +560,10 @@ extern int slurm_ckpt_restart_task(stepd_step_rec_t *job,
 	argv[3] = strdup(getenvp(job->env, "SLURM_JOB_NODELIST"));
 	argv[4] = strdup(getenvp(job->env, "SLURM_TASKS_PER_NODE"));
 	argv[5] = strdup(getenvp(job->env, "SLURM_JOB_CPUS_PER_NODE"));
-	argv[6] = strdup(getenvp(job->env, "SLURM_LOCALID"));
-	//argv[7] = strdup(getenvp(job->env, "SLURM_SRUN_COMM_HOST"));
-	argv[7] = strdup("NONE");
-	argv[8] = strdup(getenvp(job->env, "SLURM_NNODES"));
-	argv[9] = strdup(getenvp(job->env, "TMPDIR"));
-
-	argv[10] = NULL;
+	argv[6] = strdup("NONE");
+	argv[7] = strdup(getenvp(job->env, "SLURM_NNODES"));
+	argv[8] = strdup(getenvp(job->env, "TMPDIR"));
+	argv[9] = NULL;
 
 	execv(argv[0], argv);
 
